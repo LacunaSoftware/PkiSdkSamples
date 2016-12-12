c8d6276f097fd9061935520f055e189a0d0e426b
Ismael Medeiros <IsmaelM@lacunasoftware.com>
2016-12-12 12:17:34 -0200

    C# MVC: Added XML element signature; Moved css and js files and fonts to the Content/ folder;

--- MVC/MVC/App_Start/BundleConfig.cs
+++ MVC/MVC/App_Start/BundleConfig.cs
@@ -6,23 +6,25 @@ namespace MVC {
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles) {
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-							"~/Scripts/jquery-{version}.js"));
+							"~/Content/js/jquery-{version}.js",
+							"~/Content/js/jquery.blockUI.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-							"~/Scripts/jquery.validate*"));
+							"~/Content/js/jquery.validate*"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-							"~/Scripts/modernizr-*"));
+							"~/Content/js/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-						 "~/Scripts/bootstrap.js",
-						 "~/Scripts/respond.js"));
+						 "~/Content/js/bootstrap.js",
+						 "~/Content/js/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-						 "~/Content/bootstrap.css",
-						 "~/Content/site.css"));
+						 "~/Content/css/bootstrap.css",
+						 "~/Content/css/bootstrap-theme.css",
+						 "~/Content/css/site.css"));
 		}
 	}
 }
---
+++ MVC/MVC/Models/SignatureInfoModel.cs
@@ -0,0 +1,11 @@
+﻿using Lacuna.Pki;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Classes {
+	public class SignatureInfoModel {
+		public string File { get; set; }
+	}
+}
