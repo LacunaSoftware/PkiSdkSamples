151578b16f918b9eae2dbf6536c4b6d0527dedd7
Ismael Medeiros <IsmaelM@lacunasoftware.com>
2016-12-12 12:09:23 -0200

    Revert "C# MVC: Added Xml element examples; Move css and js files and fonts to Content folder;"
    This reverts commit 9461fed699a51b9681f0b7a78f17e431dbd9be43.

--- MVC/MVC/App_Start/BundleConfig.cs
+++ MVC/MVC/App_Start/BundleConfig.cs
@@ -6,25 +6,23 @@ namespace MVC {
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles) {
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-							"~/Content/js/jquery-{version}.js",
-							"~/Content/js/jquery.blockUI.js"));
+							"~/Scripts/jquery-{version}.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-							"~/Content/js/jquery.validate*"));
+							"~/Scripts/jquery.validate*"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-							"~/Content/js/modernizr-*"));
+							"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-						 "~/Content/js/bootstrap.js",
-						 "~/Content/js/respond.js"));
+						 "~/Scripts/bootstrap.js",
+						 "~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-						 "~/Content/css/bootstrap.css",
-						 "~/Content/css/bootstrap-theme.css",
-						 "~/Content/css/site.css"));
+						 "~/Content/bootstrap.css",
+						 "~/Content/site.css"));
 		}
 	}
 }
