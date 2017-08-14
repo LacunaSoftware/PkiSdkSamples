3a2dfaf6e822463688d12c6de73982784ccc2e0e
Ismael Medeiros <140083162@aluno.unb.br>
2017-08-14 20:59:53 -0300

    Added Batch Signature example;

---
+++ MVC/MVC/Api/Models/BatchSignatureStartResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Api.Models {
+    public class BatchSignatureStartResponse {
+
+        public byte[] TransferData { get; set; }
+        public string TransferDataBase64 {
+            get {
+                return TransferData != null ? Convert.ToBase64String(TransferData) : "";
+            }
+            set {
+                TransferData = !string.IsNullOrEmpty(value) ? Convert.FromBase64String(value) : null;
+            }
+        }
+
+        public byte[] ToSignHash { get; set; }
+        public string ToSignHashBase64 {
+            get {
+                return ToSignHash != null ? Convert.ToBase64String(ToSignHash) : "";
+            }
+            set {
+                ToSignHash = !string.IsNullOrEmpty(value) ? Convert.FromBase64String(value) : null;
+            }
+        }
+
+        public string DigestAlgorithmOid { get; set; }
+    }
+}
--- MVC/MVC/Global.asax.cs
+++ MVC/MVC/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MVC.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,11 +15,12 @@ namespace MVC {
 		protected void Application_Start() {
 
 			AreaRegistration.RegisterAllAreas();
-			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
 
 			Lacuna.Pki.NLogConnector.NLogLogger.Configure();
-		}
+        }
 	}
 }
