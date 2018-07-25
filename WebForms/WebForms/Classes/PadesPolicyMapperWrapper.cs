15ea81c4b671f9b7fe545376fdbbb19580e72371
Alexandre Swioklo <alexswi@hotmail.com>
2018-07-24 23:41:00 -0300

    Web Forms WebAPI first version completed

---
+++ WebForms/WebForms/Classes/PadesPolicyMapperWrapper.cs
@@ -0,0 +1,22 @@
+﻿using Lacuna.Pki;
+using Lacuna.Pki.Pades;
+
+namespace WebForms.Classes {
+	public class PadesPolicyMapperWrapper : IPadesPolicyMapper {
+
+		private IPadesPolicyMapper policyMapper;
+		private ITrustArbitrator trustArbitrator;
+
+		public PadesPolicyMapperWrapper(IPadesPolicyMapper policyMapper, ITrustArbitrator trustArbitrator) {
+			this.policyMapper = policyMapper;
+			this.trustArbitrator = trustArbitrator;
+		}
+
+		public PadesPolicySpec GetPolicy(PKCertificate certificate) {
+			var policy = policyMapper.GetPolicy(certificate);
+			policy.ClearTrustArbitrators();
+			policy.AddTrustArbitrator(trustArbitrator);
+			return policy;
+		}
+	}
+}
---
+++ WebForms/WebForms/Models/BatchSignatureStartRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebForms.Models {
+	public class BatchSignatureStartRequest {
+		public int Id { get; set; }
+		public byte[] CertContent { get; set; }
+		public string CertContentBase64
+		{
+			get { return CertContent != null ? Convert.ToBase64String(CertContent) : ""; }
+			set { CertContent = !string.IsNullOrEmpty(value) ? Convert.FromBase64String(value) : null; }
+		}
+	}
+}
