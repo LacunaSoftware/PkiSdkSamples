0fa9085faf0acbb17c6c700e23e6989e5355c79b
Danilo Bispo <danilobispogalvao@gmail.com>
2025-01-23 10:32:34 -0300

    Moved error log to environment variable set in appsettings.json

--- SignerService/Program.cs
+++ SignerService/Program.cs
@@ -5,27 +5,31 @@ using Microsoft.Extensions.Logging.EventLog;
 using Serilog;
 
 namespace SignerService {
-	public class Program {
-		public static void Main(string[] args) {
-			IHost host = Host.CreateDefaultBuilder(args)
-				.UseWindowsService(options => {
-					 options.ServiceName = "LacunaSignerService";
-				})
-				.ConfigureServices(services => {
-					 LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(services);
-					 services.AddHostedService<DirectoryWatcher>();
-					 services.AddSingleton<DocumentService>();
-				})
-				.ConfigureLogging((context, logging) => {
-					 logging.AddConfiguration(context.Configuration.GetSection("Logging"));
-				})
-				.UseSerilog((hostingContext, services, loggerConfiguration) => loggerConfiguration
-					.ReadFrom.Configuration(hostingContext.Configuration)
-					.Enrich.FromLogContext()
-					.WriteTo.File("error/log.txt", Serilog.Events.LogEventLevel.Error)
-				)
-				.Build();
-			host.Run();
-		}
-	}
+    public class Program {
+        public static void Main(string[] args) {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .UseWindowsService(options => {
+                    options.ServiceName = "LacunaSignerService";
+                })
+                .ConfigureServices(services => {
+                    LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(services);
+                    services.AddHostedService<DirectoryWatcher>();
+                    services.AddSingleton<DocumentService>();
+                })
+                .ConfigureLogging((context, logging) => {
+                    logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+                })
+                .UseSerilog((hostingContext, services, loggerConfiguration) => {
+                    var errorLogFilePath = hostingContext.Configuration.GetValue<string>("LogErrorPath");
+                    loggerConfiguration
+                        .ReadFrom.Configuration(hostingContext.Configuration)
+                        .Enrich.FromLogContext()
+                        .WriteTo.File(errorLogFilePath, Serilog.Events.LogEventLevel.Error);
+
+                    }
+                )
+                .Build();
+            host.Run();
+        }
+    }
 }
